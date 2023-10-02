119349de07341b862222a5446997caf6a1756cb9
liad7 <liadgola7@gmail.com>
2023-10-02 11:49:32 +0300

    app-footer cmp

--- cmps/app-footer.tsx
+++ cmps/app-footer.tsx
@@ -1,12 +1,32 @@
+import Image from "next/image"
 import { Carousel } from "./carousel"
+import { GemIcon } from "lucide-react"
 
 export function AppFooter() {
     return (
-        <footer className="h-96">
+        <footer>
 
             {/* carousel */}
             <Carousel />
 
+            <div className="relative top-16 h-[500px]">
+                <Image className="object-cover md:object-fill"
+                    fill
+                    src="https://res.cloudinary.com/dug85f2rg/image/upload/v1696165627/WhatsApp_Image_2023-10-01_at_16.03.59_vf2rr6.jpg"
+                    alt=""
+                />
+
+                <div className="relative text-right pr-4 pt-32">
+                        <h3 className="text-4xl mb-4">? מה אנחנו מציעים</h3>
+                        <p>מגוון סוגי הצעות <GemIcon className="inline" /></p>
+                        <p> שירות מקצועי ואדיב <GemIcon className="inline" /></p>
+                        <p>הגעה לכל מקום בארץ <GemIcon className="inline" /></p>
+                        <p>מחירים נוחים לכל כיס <GemIcon className="inline" /></p>
+                        <p>שיחות ייעוץ והכוונה <GemIcon className="inline" /></p>
+                        <p>צילום מצלמהֿֿ/רחפן <GemIcon className="inline" /></p>
+                        <p>הפקה עם זיקוקים <GemIcon className="inline" /></p>
+                </div>
+            </div>
         </footer>
     )
 }
--- cmps/app-header.tsx
+++ cmps/app-header.tsx
@@ -2,19 +2,17 @@ import Image from "next/image"
 
 export function AppHeader() {
     return (
-        <header className="h-2/3">
-            <div className="relative overflow-hidden w-full h-[600px] md:h-[650px]">
-                <Image className="object-cover md:object-fill"
+        <header>
+            <div className="relative overflow-hidden w-full h-[400px] md:h-[650px]">
+                <Image className="object-fill"
                     fill
                     src="https://res.cloudinary.com/dug85f2rg/image/upload/v1696168549/WhatsApp_Image_2023-10-01_at_16.54.34_aly9xx.jpg"
                     alt=""
                 />
             </div>
-            <p className="absolute center w-max top-0">אני אריאל בוכריס בראון</p>
+            <p className="absolute center w-max top-4 text-xl">ההצעה שתמיד רציתם <span className="text-[#ffd935]">במחיר חלומי</span></p>
 
-            <h2 className="absolute center w-max top-1/4">אני אריאל בוכריס בראון2</h2>
-
-            <button>אני רוצה הצעת מחיר</button>
+            <button className="absolute center w-max top-36 md:top-1/3 rounded-3xl bg-[#ffd935] text-sm duration-300 transition-all px-8 py-4 hover:bg-[#dab200]">אני רוצה הצעת מחיר</button>
         </header>
     )
 }
